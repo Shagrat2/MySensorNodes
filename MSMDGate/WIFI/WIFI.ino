f16b8fa5b47ca1e675bd8b64ed671b354fcc7381
i_zaitsev <ivan@jad.ru>
2017-03-12 22:59:46 +0300

    FIx str compare

--- MSMDGate/WIFI/WIFI.ino
+++ MSMDGate/WIFI/WIFI.ino
@@ -86,7 +86,7 @@ void setup() {
   WebServerInit();  
 
   //=== Config connect AP
-  APConnect = APCSSId != "";
+  APConnect = strcmp(APCSSId, "") == 0;
   APCStatus = WL_IDLE_STATUS;
   APCLastTime = millis();
 
